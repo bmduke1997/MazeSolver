3c89379d16093d0d5b6e6699db34f17cf313a1dd
Patrick Shinn <patrick_shinn@yahoo.com>
2016-09-02 21:01:23 -0400

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,82 @@
+package sample;
+
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Controller {
+    private Stage primaryStage; // the primary stage of the GUI
+    private char[][][] masterMaze;
+
+    // used to extract the primary stage from the Main Class.
+    public void setStage(Stage stage){
+        this.primaryStage = stage;
+    }
+
+    // Load method, loads the maps into a 3d array.
+    // // TODO: 9/2/16 make this load the map into the GUI 
+    public void load() {
+        FileChooser chooser = new FileChooser();
+        Scanner scanner;
+        char[][] maze = new char[0][0];
+        char[][][] masterMazeHolder = {maze};
+        int mazeCounter = 0;
+        int masterCounter = 0;
+
+        chooser.setTitle("Open a Map");
+        // sets the extension filter types
+        chooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Text Files", "*.txt"),
+                new FileChooser.ExtensionFilter("Map Files", "*.map") // custom extension, just for fun.
+        );
+
+        File map = chooser.showOpenDialog(primaryStage);
+        //// TODO: 9/2/16 Remove debug here.
+        System.out.println(map.getName());
+
+        try {
+            scanner = new Scanner(map);
+            
+
+            while (scanner.hasNextLine()){ // so while we still have lines to read
+                String line = scanner.nextLine();
+                if (line.contains("--")){ // if we have reached the end of a maze
+                    masterMazeHolder = Arrays.copyOf(masterMazeHolder, masterCounter + 1); // expand the masterMazeHolder array
+                    masterMazeHolder[masterCounter] = maze; // insert maze
+                    maze = Arrays.copyOf(maze, 0); // dump maze
+                    masterCounter ++; // increase masterCounter
+                    mazeCounter = 0; //reset mazeCounter for next run.
+
+
+                }else{ // so if we are in a level of the maze
+                    char[] row = line.toCharArray(); // split each line of the maze into an array.
+                    maze = Arrays.copyOf(maze, mazeCounter + 1); // expand the maze array
+                    maze[mazeCounter] = row; // insert into the maze array
+                    mazeCounter ++; // increase the counter.
+
+                }
+            }
+            masterMazeHolder = Arrays.copyOf(masterMazeHolder, masterCounter + 1); // expand the masterMazeHolder array one last time
+            masterMazeHolder[masterCounter] = maze; // get the last maze.
+        }catch (FileNotFoundException e){
+            //congrats, this doesnt matter... simply appeasing the compiler.
+        }
+        
+        this.masterMaze = masterMazeHolder; //makes the newly made 3D array available to other methods.
+        
+        //// TODO: 9/2/16 more debug stuff to delete 
+        for ( char[][] z : masterMazeHolder){
+            for (char[] y : z ){
+                for (char x : y){
+                    System.out.print(x);
+                }
+                System.out.println();
+            }
+            System.out.println("---------------------------------");
+        }
+    }
+}
