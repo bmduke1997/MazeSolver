87890e74b8525edb260c0e38ae16698c1eb2beda
Patrick Shinn <patrick_shinn@yahoo.com>
2016-09-09 12:34:03 -0400

    Cleaned up the ugly code in ThompsonStack

--- src/MazeLogic/ThompsonStack.java
+++ src/MazeLogic/ThompsonStack.java
@@ -1,52 +1,50 @@
 package MazeLogic;
 
-public class ThompsonStack<T>
-{
-  private Node headNode;
-  private class Node
-  {
-    private T data;
-    private Node nextNode;
-    public Node(T c)
-    {
-      data = c;
-      nextNode = null;
-    }
-  }
+class ThompsonStack<T> {
+      private Node headNode;
 
-  ThompsonStack()
-  {headNode = null;
-  }
-   boolean isEmpty(){
-    if (headNode == null)
-      return true;
-    return false;
-  }
-  public T peek()
-  {
-      return headNode.data;
-  }
-  public T pop()
-  {
-    Node tempNode = headNode;
-    headNode = headNode.nextNode;
-    return tempNode.data;
-  }
-  public void push(T data)
-  {
-    Node newNode = new Node(data);
-    newNode.nextNode = headNode;
-    headNode = newNode;
-  }
-  
-  //// TODO: 9/9/16 TEST THIS 
-  public void peekAll(){
-      Node temp = headNode;
-      Node temp2 = temp;
-      while (temp.nextNode != null){
-          temp = temp2;
-          temp2 = temp2.nextNode;
-          System.out.print(temp.data);
+      private class Node {
+            private T data;
+            private Node nextNode;
+
+            Node(T c) {
+                  data = c;
+                  nextNode = null;
+            }
       }
-  }
+
+      ThompsonStack(){
+          headNode = null;
+      }
+
+      boolean isEmpty(){
+        return  headNode == null;
+      }
+
+      T peek()
+      {
+          return headNode.data;
+      }
+
+      T pop(){
+        Node tempNode = headNode;
+        headNode = headNode.nextNode;
+        return tempNode.data;
+      }
+
+      void push(T data){
+        Node newNode = new Node(data);
+        newNode.nextNode = headNode;
+        headNode = newNode;
+      }
+
+      void peekAll(){
+          Node temp = headNode;
+          Node temp2 = temp;
+          while (temp.nextNode != null){
+              temp = temp2;
+              temp2 = temp2.nextNode;
+              System.out.print(temp.data);
+          }
+      }
 }
