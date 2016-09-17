e0983ff123d19204779c082f594202953167c2a8
Patrick Shinn <patrick_shinn@yahoo.com>
2016-09-17 00:42:41 -0400

    stuff

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -1,13 +1,17 @@
 package GUI;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -17,6 +21,7 @@ public class Main extends Application {
         controller.setStage(primaryStage);
         primaryStage.setScene(new Scene(root, 1070, 630));
         primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/graphics/AppIcon.png")));
+
         primaryStage.show();
     }
 
